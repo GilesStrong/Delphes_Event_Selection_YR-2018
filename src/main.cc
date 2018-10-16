73c57a8aa62f3596b0cf7e6ae6b7cad869dc0443
GilesStrong <giles.strong@outlook.com>
2018-10-16 15:27:39 +0100

    Adding separate gen-level selection for the filtered delphes

--- src/main.cc
+++ src/main.cc
@@ -580,14 +580,16 @@ bool getGenSystem(TClonesArray* branchParticle,
         if (tmpParticle->IsPU == true) continue;
         if (tmpParticle->Status == 22 && tmpParticle->PID == 25) {
             higgs.push_back(tmpParticle->P4());
-        } else if (tmpParticle->PID == 5) {
-            bquarks.push_back(tmpParticle->P4());
-        } else if (tmpParticle->PID == -5) {
-            anti_bquarks.push_back(tmpParticle->P4());
-        } else if (tmpParticle->PID == 15) {
-            taus.push_back(tmpParticle->P4());
-        } else if (tmpParticle->PID == -15) {
-            anti_taus.push_back(tmpParticle->P4());
+        } else if (tmpParticle->Status == 32) {
+            if (tmpParticle->PID == 5) {
+                bquarks.push_back(tmpParticle->P4());
+            } else if (tmpParticle->PID == -5) {
+                anti_bquarks.push_back(tmpParticle->P4());
+            } else if (tmpParticle->PID == 15) {
+                taus.push_back(tmpParticle->P4());
+            } else if (tmpParticle->PID == -15) {
+                anti_taus.push_back(tmpParticle->P4());
+            }
         }
     }
     if (higgs.size() != 2) {
